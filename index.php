0143c77c12b729878c6a6f752d8a3fd3c67b9057
gpuettmann <gregor.puettmann@iCloud.com>
2014-12-30 17:53:18 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <html>
 <h1>Deploy Demo Reloaded!</h1>
 <p><img src = 'https://s3.eu-central-1.amazonaws.com/deploydemo/cloudHelpDesk.jpg'></p>
-<?php include("opsworks.php") ?>
+<?php include("opsworks.php") 
+$opsworks = new Opsworks;
+print $opsworks->$db->$host;
+?>
 </html>
